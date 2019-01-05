5bf0d158a5517dbca02f0f1391343a74df6675ad
Katherine <katdom13@gmail.com>
2019-01-05 11:07:04 +0800

    Experiment with client chat

--- _017_11_UDP_MultiChat_Client.py
+++ _017_11_UDP_MultiChat_Client.py
@@ -13,15 +13,6 @@ recv()
 close()
 '''
 
-def recv_session(sock, lock):
-	while True:
-		lock.acquire()
-		server_message, addr = sock.recvfrom(1024)
-		if server_message.decode() == 'q':
-			break
-		print(server_message.decode())
-		lock.release()
-
 host = "0.0.0.0"
 port = 0
 server = ('192.168.8.100', 1234)
@@ -29,19 +20,43 @@ myLock = threading.Lock()
 
 mySock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 mySock.bind((host, port))
+mySock.setblocking(0)
 
-# set username
-username = raw_input("Enter username: ")
+SHUTDOWN = False
+
+def recv_session(sock, lock):
+	while not SHUTDOWN:
+		try:
+			lock.acquire()
+			while True:
+				data, addr = sock.recvfrom(1024)
+				print(data.decode())
+		except:
+			time.sleep(0.1)
+		finally:
+			lock.release()
+
 
 # recv data
 thread1 = threading.Thread(target=recv_session, args=(mySock, myLock))
 thread1.start()
 
+# set username
+username = raw_input("Enter username: ")
+
 #send data
 client_message = raw_input("-> ")
 while client_message != 'q':
-	client_message = username +': ' +client_message
-	mySock.sendto(client_message.encode(), server)
-	client_message = raw_input("-> ")
+	if client_message != '':
+		client_message = username +': ' +client_message
+		mySock.sendto(client_message.encode(), server)
+		myLock.acquire()
+		client_message = raw_input("-> ")
+		myLock.release()
+		time.sleep(0.1)
+
+SHUTDOWN = True
+
+thread1.join()
 
 mySock.close()
