78b8f9025c9edfd5b0949d82f27c8858d9556ec9
Katherine <katdom13@gmail.com>
2019-01-05 12:29:25 +0800

    Updated Client MultiChat (with send blocking)

--- _017_11_UDP_MultiChat_Client.py
+++ _017_11_UDP_MultiChat_Client.py
@@ -13,6 +13,31 @@ recv()
 close()
 '''
 
+def send_session(sock, server, lock):
+	client_username = raw_input("Username: ")
+	client_message = raw_input("-> ")
+	while client_message != "q":
+		client_message = client_username +": " +client_message
+		sock.sendto(client_message.encode(), server)
+		lock.acquire()
+		client_message = raw_input("-> ")
+		lock.release()
+		client_message = client_message.decode()
+		time.sleep(0.1)
+
+	sock.sendto(client_message.encode(), server)
+
+def recv_session(sock, lock):
+	while True:
+		lock.acquire()
+		data, addr = sock.recvfrom(1024)
+		data = data.decode()
+		if data == 'q':
+			break
+		print(data.decode())
+		lock.release()
+		time.sleep(0.2)
+	
 host = "0.0.0.0"
 port = 0
 server = ('192.168.8.100', 1234)
@@ -20,43 +45,16 @@ myLock = threading.Lock()
 
 mySock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 mySock.bind((host, port))
-mySock.setblocking(0)
 
-SHUTDOWN = False
+#send thread
+send_t = threading.Thread(target=send_session, args=(mySock, server, myLock))
+#recv thread
+recv_t = threading.Thread(target=recv_session, args=(mySock, myLock))
 
-def recv_session(sock, lock):
-	while not SHUTDOWN:
-		try:
-			lock.acquire()
-			while True:
-				data, addr = sock.recvfrom(1024)
-				print(data.decode())
-		except:
-			time.sleep(0.1)
-		finally:
-			lock.release()
+send_t.start()
+recv_t.start()
 
-
-# recv data
-thread1 = threading.Thread(target=recv_session, args=(mySock, myLock))
-thread1.start()
-
-# set username
-username = raw_input("Enter username: ")
-
-#send data
-client_message = raw_input("-> ")
-while client_message != 'q':
-	if client_message != '':
-		client_message = username +': ' +client_message
-		mySock.sendto(client_message.encode(), server)
-		myLock.acquire()
-		client_message = raw_input("-> ")
-		myLock.release()
-		time.sleep(0.1)
-
-SHUTDOWN = True
-
-thread1.join()
+send_t.join()
+recv_t.join()
 
 mySock.close()
